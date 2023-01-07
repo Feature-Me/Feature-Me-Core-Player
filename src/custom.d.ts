4f2462487bbeccdb117adb50f5e13bcc0ad4eea4
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-08 00:08:54 +0900

    add config

---
+++ src/custom.d.ts
@@ -0,0 +1,23 @@
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.svg";
+declare module "*.ico";
+declare module "*.mp3";
+declare module '*.scss' {
+    const exports: {
+        [exportName: string]: string
+    };
+    export = exports
+}
+declare module '*.css' {
+    const exports: {
+        [exportName: string]: string
+    };
+    export = exports
+}
+
+
+//phina.js module
+declare module 'phina.js';
