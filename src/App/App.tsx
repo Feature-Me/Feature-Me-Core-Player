5b615a84399fcd49f0bff797c387206cf90ceab9
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-09 04:01:02 +0900

    remov modules and add react strictmode

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import initLocalStorage from "Utils/Storage/LocalStorage/initLocalStorage";
 import { HashRouter } from "react-router-dom";
-import * as THREE from "three";
-import * as PIXI from "pixi.js";
-import * as phina from "phina.js";
+import {REVISION as threeRevision} from "three";
+import {VERSION as pixiVesion} from "pixi.js";
 
 import ErrorBoundary from "Components/errorBoundary/errorBoundary";
 import PageRouter from "Routs/router";
@@ -15,7 +14,6 @@ import style from "./App.scss";
 
 
 function App(): JSX.Element {
-
     return (
         <HashRouter>
             <div className={style.app}>
@@ -25,13 +23,14 @@ function App(): JSX.Element {
     )
 }
 
-
 function render(): void {
     const container: HTMLDivElement = document.querySelector("#root")!;
     createRoot(container).render(
-        <ErrorBoundary>
+        <React.StrictMode>
+            <ErrorBoundary>
             <App />
         </ErrorBoundary>
+        </React.StrictMode>
     );
 }
 
@@ -45,7 +44,8 @@ function init(): void {
     console.log("%c Please be careful when you paste anything to console.\n Attackers may steal your information.", "color:#ffa69f;background-color:#633023;font-size:16px;");
     console.log("%c  Do you know what you are supposed to do with this tool? If you do, how about contributing to this project?", "color:#1189da;font-size:24px;");
     console.log(`%c Running Feature Me ${version.version}.`, "color:#a0d6a6;background-color:#2d3c2e;font-size:16px;");
-    console.log(`%c React${React.version}\n THREE.JS${THREE.REVISION}\n Pixijs${PIXI.VERSION}\n Phina.js${phina.VERSION}`, "color:#f3c0e6;background-color:#3c2d38;font-size:16px;");
+    console.log(`%c React${React.version}\n THREE.JS${threeRevision}\n Pixijs${pixiVesion}\n`, "color:#f3c0e6;background-color:#3c2d38;font-size:16px;");
+    
 
 }
 window.addEventListener('load', init);
