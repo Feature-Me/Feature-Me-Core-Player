60e217b2b0f453e4d3e57e3c1be5740429ea99c5
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-09 04:02:59 +0900

    update loalstorage initializer

---
+++ src/Utils/Storage/LocalStorage/default/monaco.settings.ts
@@ -0,0 +1,9 @@
+const defaultSettings: defaultMonacoSettings = {
+    cursorStyle: "line-thin",
+    cursorBlinking: "smooth",
+    fontSize: 14,
+    renderWhitespace: "all",
+    smoothScrolling: true,
+}
+
+export default defaultSettings;
--- src/Utils/Storage/LocalStorage/initLocalStorage.tsx
+++ src/Utils/Storage/LocalStorage/initLocalStorage.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import { toast } from "react-toastify";
+import defaultSettings from "./default/monaco.settings";
 
 function initLocalStorage(): void {
     try {
-        
+        localStorage.setItem("monacoSettings", JSON.stringify({ ...defaultSettings, ...JSON.parse(localStorage.getItem("monacoSettings") || "{}") }))
     } catch (error) {
         console.error(error);
-        toast.error(`${error}`);
     }
 }
 
