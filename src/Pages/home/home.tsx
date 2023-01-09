de19137983e37b782efe5580c8d26239282a4fb9
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-09 19:00:09 +0900

    update home page

--- src/Pages/home/home.tsx
+++ src/Pages/home/home.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { HashRouter } from "react-router-dom";
-import ChamferedButton from "Components/Button/chamferedButton/chamferedButton";
+import { useAtomValue } from "jotai";
 
-import style from "./home.scss";
 import AudioCard from "./audioCard/audioCard";
 import ChartData from "./chartData/chartData";
 import Header from "Components/Header/header";
+import EngineSelector from "./selectEngine/selectEngine";
 
+import editorState from "State/editorState";
+
+import style from "./home.scss";
 
 const Home: React.FC = () => {
+    const { maximized } = useAtomValue(editorState);
+
     return (
         <div className={style.home}>
             <Header />
-            <div className={style.content}>
+            <div className={`${style.content} ${maximized && style.editorMaximized}`}>
                 <div className={style.editor}>
                     <AudioCard />
                     <ChartData />
+                </div>
+                <div className={style.selectEngine}>
+                    <h2>Select Game Engine</h2>
+                    <EngineSelector />
                 </div>
             </div>
         </div>
