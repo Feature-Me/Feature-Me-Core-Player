c2386bcef555a067d0b1af9a7f00cbe7dc327652
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-09 16:29:54 +0900

    update debug log

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -44,7 +44,7 @@ function init(): void {
     console.log("%c Please be careful when you paste anything to console.\n Attackers may steal your information.", "color:#ffa69f;background-color:#633023;font-size:16px;");
     console.log("%c  Do you know what you are supposed to do with this tool? If you do, how about contributing to this project?", "color:#1189da;font-size:24px;");
     console.log(`%c Running Feature Me ${version.version}.`, "color:#a0d6a6;background-color:#2d3c2e;font-size:16px;");
-    console.log(`%c React${React.version}\n THREE.JS${threeRevision}\n Pixijs${pixiVesion}\n`, "color:#f3c0e6;background-color:#3c2d38;font-size:16px;");
+    console.log(`%c React${React.version}\n THREE.JS${threeRevision}\n Pixijs${pixiVesion}\n Phina.js 0.2.3`, "color:#f3c0e6;background-color:#3c2d38;font-size:16px;");
     
 
 }
