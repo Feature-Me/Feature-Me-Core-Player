0fcd3f5d223d21cc4346c4ae6ff92af6e901a91b
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-09 16:30:15 +0900

    add link wrapper

---
+++ src/Components/linkWrapper/linkWrapper.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+interface linkWrapperPropsType {
+    children?: React.ReactNode,
+    to: string,
+    className?: string,
+    state?:any;
+}
+
+const LinkWrapper: React.FC<linkWrapperPropsType> = (props): JSX.Element => {
+    const navigate = useNavigate();
+
+    function handleClick(e: React.MouseEvent<HTMLDivElement>): void {
+        navigate(props.to,{state:props.state});
+    }
+
+    return (
+        <div onClick={handleClick} className={props.className}>
+        {props.children}
+        </div>
+    )
+}
+
+export default LinkWrapper;
