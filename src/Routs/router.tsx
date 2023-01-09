546ce79a14206066935f848c6dd2fd06d2cb193f
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-09 16:31:00 +0900

    update router

--- src/Routs/router.tsx
+++ src/Routs/router.tsx
@@ -1,11 +1,14 @@
-import Home from "Pages/home/home";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import Home from "Pages/home/home";
+import lazyLoad from "Utils/lazyLoad/lazyLoad";
+const Converter  = lazyLoad(()=>import("Pages/converter/converter"))
 const PageRouter: React.FC = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/convert" element={<Converter />} />
         </Routes>
     )
 }
